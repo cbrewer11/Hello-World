7844ee4a317c9c5d8356cbd3a0000c2acfd9fbde
cbrewer11 <54644869+cbrewer11@users.noreply.github.com>
2019-09-13 18:18:28 -0500

    Create HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+/**
+* Author: Chandler Brewer
+* Date: 2019/9/10
+*
+* Assignment 1 part 1, bearing from location a to b program
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char **argv) {
+printf("Chandler Brewer");
+}
